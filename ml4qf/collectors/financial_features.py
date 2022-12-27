f610bb707f4b82e2370cd48f1fe0863333330867
Alvaro Cea <alvaro_cea@outlook.com>
2022-12-27 20:08:51 +0000

    more work towards setting a basic structure

--- ml4qf/collectors/financial_features.py
+++ ml4qf/collectors/financial_features.py
@@ -2,35 +2,90 @@
 import numpy as np
 import os
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import timedelta
 from datetime import date
 import pandas_datareader as pdr
 from collections import defaultdict
+import yfinance as yf
 
-def add_returns(self, df, price='Close', log_return=False, *args, **kwargs):
-    """
-    Add the returns to a data frame with prices.
-    """
-    #self.df.rename(columns={price: 'price'})
-    if not log_return:
-        df['return'] = df[price].pct_change()
-    else:
-        df['return'] = np.log(df['price'] /
-                              df['price'].shift(periods=1))
+class FinancialData:
+
+    def __init__(self, inputs, df=None, **kwargs):
+
+        if df is None:
+            self.df = self.get_data(inputs.TICKER, inputs.YEAR0,
+                                    inputs.MONTH0, inputs.DAY0, inputs.NUM_DAYS)
+            self.df['price'] = self.df[inputs.PRICE]
+            self.add_returns(inputs.LOG_RETURN)
+        else:
+            self.df = df
+        if len(inputs.FEATURES) > 0:
+            self.features = FeaturesPrice(self.df, **inputs.FEATURES)
+
+    def add_returns(self, log_return):
+        """Add the returns to a data frame with prices."""
+        if not log_return:
+            self.df['return'] = self.df['price'].pct_change()
+        else:
+            self.df['return'] = np.log(self.df['price'] /
+                                       self.df['price'].shift(periods=1))
+
+    @classmethod
+    def clone(cls, inputs, df):
+        return cls(inputs, df.copy())
+
+    @staticmethod
+    def get_data(ticker, year, month, day, num_days):
+        """
+
+
+        Parameters
+        ----------
+        ticker :
+
+        year :
+
+        month :
+
+        day :
+
+        num_days :
+
+
+        Returns
+        -------
+        out :
+
+        """
+        date_end = date(year, month, day)
+        date_start = date_end - timedelta(**{'days':num_days})
+        data_folder = os.getcwd()+'/data'
+        data_file = data_folder + f"/{ticker}_{date_start}_{date_end}"
+
+        if not os.path.isdir(data_folder):
+            print("***** Creating data folder *****")
+            os.makedirs(data_folder)
+        if os.path.isfile(data_file):
+            print("***** Loading data from csv file *****")
+            df = pd.read_csv(data_file, index_col=0, parse_dates=True)
+        else:
+            print("***** Loading data from Yahoo Finance *****")
+            df = yf.download(ticker, start=date_start, end=date_end)
+            df.to_csv(data_file)
+        return df
 
 
 class FeaturesPrice:
-    """
-    Creates financial features using arbitrary rolling windows.
-    """
-    
-    def __init__(self, df: pd.DataFrame, features: dict = None):
-        self.df = df
-        self.features_in = features
+    """Creates financial features using arbitrary rolling windows."""
+
+    def __init__(self, df: pd.DataFrame, **features: dict):
+        self.df = df.copy()
+        self.features_in = features.keys()
         self.features_list = []
-        if features is not None:
-            self.get_features(**features)
-        
+        self.get_features(**features)
+        self.drop_nonfeatures()
+        self.drop_nans()
+
     def get_features(self, **kwargs):
         """
         Below code features to an existing data frame
@@ -47,9 +102,13 @@ class FeaturesPrice:
             else:
                 raise ValueError('Invalid value for feature')
 
+
     def drop_nans(self):
         """Drop NaN values in the dataframe."""
         self.df.dropna(inplace=True)
+
+    def drop_nonfeatures(self):
+        self.df = self.df[self.features_list]
 
     def add_feature(self, label, period=None):
         _type = getattr(self, label)
@@ -61,17 +120,17 @@ class FeaturesPrice:
             self.features_list.append(f"{label}{period}d")
 
     def return_(self, df, period):
-        
+
         out = df['return'].shift(periods=period)
         return out
 
     def Ret_(self, df, period):
-        
+
         out = df['return'].rolling(period).sum()
         return out
-    
+
     def Std_(self, df, period):
-        
+
         out = df['return'].rolling(period).std()
         return out
 
@@ -116,7 +175,7 @@ class FeaturesPrice:
         return out
 
 def features_label(features_list):
-    
+
     features_dict = defaultdict(list)
     for _ in features_list:
         feature_= _.split('_')
@@ -124,6 +183,6 @@ def features_label(features_list):
         di = feature_[-1]
         if di != '':
             features_dict[feature + '_'].append(int(di[:-1]))
-        else:    
+        else:
             features_dict[feature + '_'] = None
     return features_dict
