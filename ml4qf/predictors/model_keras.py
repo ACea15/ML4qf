2a6fd226ef9da161830a18b2e71972281771d0a3
Alvaro Cea <alvaro_cea@outlook.com>
2023-01-11 16:32:32 +0000

    building overall strategy in airbus.org

--- ml4qf/predictors/model_keras.py
+++ ml4qf/predictors/model_keras.py
@@ -13,7 +13,6 @@ from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 import tensorflow as tf
 
-from sklearn.base import BaseEstimator
 def to_dict(*args):
 
     out = list()
