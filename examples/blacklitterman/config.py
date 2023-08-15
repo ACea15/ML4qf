e4dd977f6e878c11f1ccd372944d651e4f0d01b6
Alvaro Cea <alvaro_cea@outlook.com>
2023-08-15 16:08:06 +0100

    work on ARIMA and uncorrelated portfolio selection

--- examples/blacklitterman/config.py
+++ examples/blacklitterman/config.py
@@ -18,15 +18,16 @@ ASSET_SELECTION_PCT = dict(largest=(0.05, 1),
 #                            medium=(0.5, 3)
 #                            )
 
-# ASSET_SELECTION_NAMES = ['JPM',
-#                          'NFLX',
-#                          'ANET',
-#                          'PH',  
-#                          'ROST',
-#                          'UDR', 
-#                          'RVTY',
-#                          'CAG',
-#                          'NRG']
+ASSET_SELECTION_NAMES = ['JPM',     #   AMZN
+                         'CVS',#'CMCSA',#'LOW',#'CVS',     #  CVS 
+                         'ATVI',    #   ATVI
+                         'PH',      #   AFL 
+                         'WELL',    #   PAYX
+                         'YUM',#'DXCM',    #   WELL
+                         'KR',      #  KR  
+                         'ATO',     #  ATO 
+                         'EQT',#'GEN',     #   GEN 
+                         'DXC']     #   RHI 
 # KO  
 # LOW 
 # GS  
@@ -37,17 +38,39 @@ ASSET_SELECTION_PCT = dict(largest=(0.05, 1),
 # ESS 
 # DXC
 
-ASSET_SELECTION_NAMES = None
-start_date_factors = "2011-03-31"
+#x JPM  
+# CMCSA
+# ADI  
+# PH   
+#x DXCM 
+#x TSCO 
+#x AEE  
+#x CAG  
+# CE   
+# FRT  
+
+# AMZN
+# CVS 
+# ATVI
+# AFL 
+# PAYX
+# WELL
+# KR  
+# ATO 
+# GEN 
+# RHI 
+#ASSET_SELECTION_NAMES = None
+
+start_date_factors = "2000-03-31"
 end_date_factors =  "2022-12-31"
 ###
-start_date_assets = "2011-02-01"
+start_date_assets = "2000-02-01"
 end_date_assets = "2022-12-01"
 
 start = datetime.datetime.strptime(start_date_assets, "%Y-%m-%d")
 end = datetime.datetime.strptime(end_date_assets, "%Y-%m-%d")
 days = (end - start).days
-train_test_ratio = 0.8
+train_test_ratio = 0.9
 split_data_idx = int(train_test_ratio * days * 12 / 365)
 
 lambda_mkt = 2.24
--- ml4qf/predictors/model_stats.py
+++ ml4qf/predictors/model_stats.py
@@ -23,19 +23,25 @@ def arima_pred(x_train, num_steps, arima_parameters, fit_sett=None):
         history.append(yhat)
     return np.array(predictions)
 
-def arima_fit(X, model_names, arima_parameters, fit_sett=None):
+def arima_fit(X, model_names, arima_parameters,
+              model_sett=None, fit_sett=None):
     if fit_sett is None:
         fit_sett = dict()
+    if model_sett is None:
+        model_sett = dict()
     arima_models = dict()
     if isinstance(arima_parameters, dict):
         parameters_asdict = True
     else:
         parameters_asdict = False
     for i, k in enumerate(model_names):
+        print(f"Fitting ARIMA for {k}")
         if parameters_asdict:
-            model_k = ARIMA(X[:, i], order=arima_parameters[k])
+            model_k = ARIMA(X[:, i], order=arima_parameters[k],
+                            **model_sett)
         else:
-            model_k = ARIMA(X[:, i], order=arima_parameters)
+            model_k = ARIMA(X[:, i], order=arima_parameters,
+                            **model_sett)
         model_fit = model_k.fit(**fit_sett)
         arima_models[k] = model_fit
     return arima_models
@@ -49,9 +55,9 @@ def arima_build_pred(arima_model, Xtrain, Xtest,
     len_test = len(Xtest)    
     for i, k in enumerate(model_names):
         train_dict[k] = Xtrain[:, i]
-        train_dict[k+"_pred"] = arima_model.predict(0, len_train-1)
+        train_dict[k+"_pred"] = arima_model[k].predict(0, len_train-1)
         test_dict[k] = Xtest[:, i]
-        test_dict[k+"_pred"] = arima_model.predict(len_train, len_train + len_test -1)
+        test_dict[k+"_pred"] = arima_model[k].predict(len_train, len_train + len_test -1)
     df_train = pd.DataFrame(train_dict, index=index_train)
     df_test = pd.DataFrame(test_dict, index=index_test)
     return df_train, df_test
